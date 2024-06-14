238a2d10143abffe700c35695b1269e8c2f100f6
Akhila1331 <149419227+Akhila1331@users.noreply.github.com>
2024-06-15 00:00:27 +0530

    Add files via upload
    Springboottrig

---
+++ Trig/src/main/java/com/example/demo/Calculator.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+public class Calculator {
+	private int id;
+	private int angle;
+	private String func;
+	private double result;
+	public Calculator(int id, int angle, String func, double result) {
+		super();
+		this.id = id;
+		this.angle = angle;
+		this.func = func;
+		this.result = result;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getAngle() {
+		return angle;
+	}
+	public void setAngle(int angle) {
+		this.angle = angle;
+	}
+	public String getFunc() {
+		return func;
+	}
+	public void setFunc(String func) {
+		this.func = func;
+	}
+	public double getResult() {
+		return result;
+	}
+	public void setResult(double result) {
+		this.result = result;
+	}
+	
+
+}
---
+++ Trig/src/main/java/com/example/demo/TrigController.java
@@ -0,0 +1,67 @@
+package com.example.demo;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class TrigController {
+	@Autowired
+	private JdbcTemplate template;
+
+	@GetMapping("/")
+		public String index() {
+			return "index.jsp";
+		}
+
+		@RequestMapping("/calculate")
+		public String calculate(Model model, @RequestParam("angle") int angle, @RequestParam("func") String func) {
+
+			double result = 0.0;
+
+			switch(func) {
+				case "sin":
+					result = Math.sin(Math.toRadians(angle));
+					break;
+				case "cos":
+					result = Math.cos(Math.toRadians(angle));
+					break;
+				case "tan":
+					result = Math.tan(Math.toRadians(angle));
+					break;
+				case "sec":
+					result = 1 / (Math.cos(Math.toRadians(angle)));
+					break;
+				case "cosec":
+					result = 1 / (Math.sin(Math.toRadians(angle)));
+					break;
+				case "cot":
+					result = 1 / (Math.tan(Math.toRadians(angle)));
+					break;
+			}
+			String sql = "insert into trig (angle, func, result) values (?,?,?)";
+			template.update(sql, angle, func, result);
+			
+			model.addAttribute("angle", angle);
+			model.addAttribute("func", func);
+			model.addAttribute("result", result);
+			return "result.jsp";
+			
+		}
+		@RequestMapping("records")
+		public String records(Model model) {
+			String sql="select * from trig";
+			List<Calculator>records=template.query(sql,(rs,rowNum)->new Calculator(rs.getInt("id"),
+					rs.getInt("angle"),
+					rs.getString("func"),
+					rs.getDouble("result")));
+		
+		model.addAttribute("records", records);
+		return "records.jsp";
+	}
+}
